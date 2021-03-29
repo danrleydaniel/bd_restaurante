a5a7694c91902d832aa7667632c7afc4ae3d5694
danrleydaniel <danrleydaniel21@gmail.com>
2021-03-29 16:37:16 -0300

    inserindo valores na tabela cliente

---
+++ docs/inserts.sql
@@ -0,0 +1,29 @@
+INSERT INTO Cliente(Codigo, CPF, Forma_pagamento, Nome)
+VALUES
+(
+1, '20155971409', 'crédito', 'Ana'
+);
+
+INSERT INTO Cliente(Codigo, CPF, Forma_pagamento, Nome)
+VALUES
+(
+2, '52297791445', 'débito', 'Danrley'
+);
+
+INSERT INTO Cliente(Codigo, CPF, Forma_pagamento, Nome)
+VALUES
+(
+3, '99607213459', 'débito', 'Gabriel'
+);
+
+INSERT INTO Cliente(Codigo, CPF, Forma_pagamento, Nome)
+VALUES
+(
+4, '79081997475', 'espécie', 'Hiago'
+);
+
+INSERT INTO Cliente(Codigo, CPF, Forma_pagamento, Nome)
+VALUES
+(
+5, '26006335450', 'crédito', 'Marcelo'
+);
